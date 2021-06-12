6a0a629a7a20e4fe96c8f378eb09cdc4ff821ba0
filipe11402 <filipe11402@outlook.pt>
2021-06-12 21:39:56 +0100

    Added register page and view

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,8 +1,15 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 from .models import User
 
 accounts = Blueprint('accounts', __name__)
 
-@accounts.route('/login')
+@accounts.route('/login', methods=['POST', 'GET'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        print("EMAIL: {}".format(email))
+
     return render_template('auth/login.html')
+@accounts.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('auth/register.html')
